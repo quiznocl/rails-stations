0a03e96978f0c466073def60b929945519fdb3fa
ema <quiznocl>
2022-10-02 12:36:06 +0900

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'movies/index'
+  end
   get 'movies' => 'movies#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get 'movies' => 'movies#index'
+  end
 end
